f85acede3c034b59c45fadbbf1157c925c5fa961
Puremba Moirangthem <moirangthem.work@gmail.com>
2024-11-28 08:06:34 +0530

    added links for meta certification

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
+      {/* <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div> */}
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <Navbar/>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -3,13 +3,18 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a motivated and technically skilled recent MCA graduate with a 7.15 CGPA, specializing in front-end development and React Js. Completed Meta-certified courses in Advanced React and JavaScript programming. Gained practical experience through internships, contributing to web accessibility projects for government websites and developing an employee payroll database management system. Published IEEE research paper on Smart Cities in India. Eager to apply strong problem-solving skills and passion for cutting-edge technologies to drive innovation in software development.`;
+export const HERO_CONTENT = (
+  <p>
+    I am a motivated and technically skilled recent MCA graduate with a 7.15 CGPA, specializing in front-end development and React Js. Certified by Meta for Professional <a href="https://coursera.org/verify/professional-cert/1O0PNSVP2QB6">Meta Front-End Developer</a>. Gained practical experience through internships, contributing to web accessibility projects for government websites and developing an employee payroll database management system. Published IEEE research paper on Smart Cities in India. Eager to apply strong problem-solving skills and passion for cutting-edge technologies to drive innovation in software development.
+  </p>
+);
 
-export const ABOUT_TEXT = `I am a passionate front-end developer and recent MCA graduate with a strong academic record (7.15 CGPA). Specialized in React.js and certified by Meta in Advanced React and JavaScript programming. My experience includes enhancing web accessibility for government sites and developing an employee payroll system during internships. As a published researcher with an IEEE paper on Smart Cities in India, I bring a unique perspective to software development. Driven by a love for cutting-edge tech and equipped with robust problem-solving skills, I'm ready to contribute to innovative projects in the field.`;
+
+export const ABOUT_TEXT = `I am a passionate front-end developer and recent MCA graduate with a strong academic record (7.15 CGPA). Specialized in React.js and certified by Meta for "Meta Front-End Developer" . My experience includes enhancing web accessibility for government sites and developing an employee payroll system during internships. As a published researcher with an IEEE paper on Smart Cities in India, I bring a unique perspective to software development. Driven by a love for cutting-edge tech and equipped with robust problem-solving skills, I'm ready to contribute to innovative projects in the field.`;
 
 export const EXPERIENCES = [
   {
-    year: "June 2023 - August 2023",
+    year: "April 2021 - July 2021",
     role: "Software Developer Intern",
     company: "Lamzing Technologies Pvt. Ltd., Imphal",
     description: `Contributed to web accessibility improvements for government websites   Collaborated with a team of 5 developers to implement responsive design features`,
@@ -22,7 +27,7 @@ export const PROJECTS = [
     title: "E-Commerce Website",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+      "A demo e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
@@ -51,5 +56,5 @@ export const PROJECTS = [
 export const CONTACT = {
   address: "B-44, Safdarjung Enclave, New Delhi, 110011 ",
   phoneNo: "+91-9967644829",
-  email: "moirangthem.test@gmail.com",
+  email: "moirangthem.work@gmail.com",
 };
